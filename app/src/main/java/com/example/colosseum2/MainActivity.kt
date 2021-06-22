f7295e618e83b539e468add1697cc9f055e90dc3
Sohee <sohee3480@gmail.com>
2021-06-22 19:28:36 +0900

    BaseActicity 만들기

--- app/src/main/java/com/example/colosseum2/MainActivity.kt
+++ app/src/main/java/com/example/colosseum2/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.colosseum2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
     }
 }
