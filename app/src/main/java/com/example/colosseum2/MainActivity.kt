f3e0372cb10cf851a40f039e828c0c175466f834
Sohee <sohee3480@gmail.com>
2021-06-23 21:07:56 +0900

    입력한 이메일과 비밀번호를 변수에 저장

--- app/src/main/java/com/example/colosseum2/MainActivity.kt
+++ app/src/main/java/com/example/colosseum2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.colosseum2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -11,6 +12,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+
+//            서버에 실제 회원이 맞는지 확인 요청 (Request)
+        }
     }
 
     override fun setValues() {
