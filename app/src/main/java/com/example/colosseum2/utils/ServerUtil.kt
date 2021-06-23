4da3406629a814567567b6aabd865fb669fe328b
Sohee <sohee3480@gmail.com>
2021-06-23 21:24:09 +0900

    로그인 전용 주소 변수 + POST 방식으로 폼데이터에 파라미터 넣어주기

--- app/src/main/java/com/example/colosseum2/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum2/utils/ServerUtil.kt
@@ -1,11 +1,26 @@
 package com.example.colosseum2.utils
 
-class ServerUtil {
+import okhttp3.FormBody
+
+class ServerUtil() {
 //    서버 연결 전담
 
     val BASE_URL = "http://54.180.52.26"
 
-    fun postRequestLogin() {
+    fun postRequestLogin(email: String, pw: String) {
+
+//        서버에 입력받은 email, pw 전달 => 로그인 기능 POST/user로 전달 => 요청(Request) 실행
+//        라이브러리(OkHttp) 활용
+//        http://54.180.52.26/user + POST + 파라미터들 첨부
+
+//        호스트 부소 + 기능 주소 결합
+        val urlString = "${BASE_URL}/user"
+
+//        POST 방식 => 파라미터를 폼데이터(폼바디)에 담아주자
+        val formData = FormBody.Builder()
+                .add("email", email)
+                .add("password", pw)
+                .build()
 
     }
 
