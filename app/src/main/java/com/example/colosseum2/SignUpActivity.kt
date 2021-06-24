f6ad3998f9a6b5880a647cdd9fa4b76270f22b10
Sohee <sohee3480@gmail.com>
2021-06-24 19:55:27 +0900

    회원가입 성공 시 닉네임 추출해서 토스트로 띄우기

--- app/src/main/java/com/example/colosseum2/SignUpActivity.kt
+++ app/src/main/java/com/example/colosseum2/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.example.colosseum2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.colosseum2.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import org.json.JSONObject
@@ -25,8 +26,28 @@ class SignUpActivity : BaseActivity() {
 
             ServerUtil.putRequestSignUp(inputEmail, inputPw, inputNickname, object : ServerUtil.Companion.JsonResponseHandler {
                 override fun onResponse(jsonObj: JSONObject) {
+                    val code = jsonObj.getInt("code")
+
+                    if(code == 200){
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val userObj = dataObj.getJSONObject("user")
+                        val nickName = userObj.getString("nick_name")
+                        
+                        runOnUiThread{
+                            Toast.makeText(mContext, "${nickName}님 환영합니다.", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+
+                    }else{
+                        val message = jsonObj.getString("message")
+
+                        runOnUiThread{
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
 
                 }
+
 
             })
         }
