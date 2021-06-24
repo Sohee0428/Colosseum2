d135a661878a6391be1dbc576e7a70b947371ede
Sohee <sohee3480@gmail.com>
2021-06-24 19:42:24 +0900

    회원가입 함수 추가 + 회원가입 화면에서 활용

--- app/src/main/java/com/example/colosseum2/SignUpActivity.kt
+++ app/src/main/java/com/example/colosseum2/SignUpActivity.kt
@@ -2,6 +2,9 @@ package com.example.colosseum2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.colosseum2.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -13,6 +16,20 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        signUpBtn.setOnClickListener {
+
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+            val inputNickname = nickNameTxt.text.toString()
+
+            ServerUtil.putRequestSignUp(inputEmail, inputPw, inputNickname, object : ServerUtil.Companion.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                }
+
+            })
+        }
     }
 
     override fun setValues() {
--- app/src/main/java/com/example/colosseum2/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum2/utils/ServerUtil.kt
@@ -63,6 +63,56 @@ class ServerUtil() {
 
 
         }
+
+        fun putRequestSignUp(email:String, pw: String, nickname: String, handler: JsonResponseHandler?) {
+
+//        서버에 입력받은 email, pw, nickname 전달 => 로그인 기능 PUT/user로 전달 => 요청(Request) 실행
+//        라이브러리(OkHttp) 활용
+//        http://54.180.52.26/user + PUT + 파라미터들 첨부
+
+//        호스트 부소 + 기능 주소 결합
+            val urlString = "${BASE_URL}/user"
+
+//        POST 방식 => 파라미터를 폼데이터(폼바디)에 담아주자
+            val formData = FormBody.Builder()
+                .add("email", email)
+                .add("password", pw)
+                .add("nick_name", nickname)
+                .build()
+
+//        어디, 어떻게, 무엇을 들고갈지
+            val request = Request.Builder()
+                .url(urlString)
+                .put(formData)
+                .build()
+
+//        이후에는 동일함.
+//        클라이언트로써의 동작 : Request 요청 => OkHttp  라이브러리 지원
+            val client = OkHttpClient()
+
+//        실제로 서버에 요청 날리기
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+//                서버에 연결 자체를 실패한 경우 (서버 마비, 인터넷 단선 등)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+//                로그인 성공 / 실패 (연결 성공 -> 검사 실패) -> 응답이 돌아온 경우
+
+                    val bodyString = response.body!!.string()
+
+                    val jsonObj = JSONObject(bodyString)
+
+                    Log.d("응답 본문", jsonObj.toString())
+
+//                    handler 변수가 null이 아니면 내용 띄우기
+                    handler?.onResponse(jsonObj)
+                }
+
+            })
+
+
+        }
     }
 }
 
