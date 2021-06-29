8039629e7bd677094cc4df240f3f9b5d4d209bad
Sohee <sohee3480@gmail.com>
2021-06-29 17:18:32 +0900

    어댑터 추가

---
+++ app/src/main/java/com/example/colosseum2/adapters/TopicAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.colosseum2.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.colosseum2.R
+import com.example.colosseum2.datas.Topic
+
+class TopicAdapter(val mContext: Context, val resId: Int, mList: List<Topic>) : ArrayAdapter<Topic>(mContext, resId, mList){
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.topic_list_item, null)
+        }
+
+        val row =tempRow!!
+
+        return row
+    }
+}
