412cfb6138ff82408ea7ba38cd9cdbaba4d9f310
Sohee <sohee3480@gmail.com>
2021-06-30 21:04:46 +0900

    Glide 등 활용 -> 실제 토론 주제 목록 데이터 표시

--- app/src/main/java/com/example/colosseum2/adapters/TopicAdapter.kt
+++ app/src/main/java/com/example/colosseum2/adapters/TopicAdapter.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.colosseum2.R
 import com.example.colosseum2.datas.Topic
+import org.w3c.dom.Text
 
-class TopicAdapter(val mContext: Context, val resId: Int, mList: List<Topic>) : ArrayAdapter<Topic>(mContext, resId, mList){
+class TopicAdapter(val mContext: Context, resId: Int, val mList: List<Topic>) : ArrayAdapter<Topic>(mContext, resId, mList){
 
     val mInflater = LayoutInflater.from(mContext)
 
@@ -23,6 +27,15 @@ class TopicAdapter(val mContext: Context, val resId: Int, mList: List<Topic>) : 
         }
 
         val row =tempRow!!
+
+        val data = mList[position]
+
+        val topicImg = row.findViewById<ImageView>(R.id.topicImg)
+        val topicTitleTxt = row.findViewById<TextView>(R.id.topicTitleTxt)
+
+        topicTitleTxt.text = data.title
+
+        Glide.with(mContext).load(data.imageURL).into(topicImg)
 
         return row
     }
