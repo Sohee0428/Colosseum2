38128f89d3e3f003ad631e0696ea2725006ac491
Sohee <sohee3480@gmail.com>
2021-07-06 22:32:50 +0900

    api 통신을 통해 의견을 등록하는 함수 만들기 (post)

--- app/src/main/java/com/example/colosseum2/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum2/utils/ServerUtil.kt
@@ -283,6 +283,55 @@ class ServerUtil() {
 
         }
 
+        fun postRequestReply(context: Context, topicId: Int, content: String, handler: JsonResponseHandler?) {
+
+//        서버에 입력받은 email, pw 전달 => 로그인 기능 POST/user로 전달 => 요청(Request) 실행
+//        라이브러리(OkHttp) 활용
+//        http://54.180.52.26/user + POST + 파라미터들 첨부
+
+//        호스트 부소 + 기능 주소 결합
+            val urlString = "${BASE_URL}/topic_reply"
+
+//        POST 방식 => 파라미터를 폼데이터(폼바디)에 담아주자
+            val formData = FormBody.Builder()
+                .add("side_id", topicId.toString())
+                .add("content", content.toString())
+                .build()
+
+//        어디, 어떻게, 무엇을 들고갈지
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+                .header("X-Http-Token", ContextUtil.getToken(context))
+                .build()
+
+//        클라이언트로써의 동작 : Request 요청 => OkHttp  라이브러리 지원
+            val client = OkHttpClient()
+
+//        실제로 서버에 요청 날리기
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+//                서버에 연결 자체를 실패한 경우 (서버 마비, 인터넷 단선 등)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+//                로그인 성공 / 실패 (연결 성공 -> 검사 실패) -> 응답이 돌아온 경우
+
+                    val bodyString = response.body!!.string()
+
+                    val jsonObj = JSONObject(bodyString)
+
+                    Log.d("응답 본문", jsonObj.toString())
+
+//                    handler 변수가 null이 아니면 내용 띄우기
+                    handler?.onResponse(jsonObj)
+                }
+
+            })
+
+
+        }
+
     }
 }
 
