9d1f82c30e9fd8667611549b82e392c6dfc475e8
Sohee <sohee3480@gmail.com>
2021-07-06 22:51:19 +0900

    의견 작성하기 기능 마무리

--- app/src/main/java/com/example/colosseum2/EditReplyActivity.kt
+++ app/src/main/java/com/example/colosseum2/EditReplyActivity.kt
@@ -2,8 +2,11 @@ package com.example.colosseum2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.colosseum2.datas.Side
+import com.example.colosseum2.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_edit_reply.*
+import org.json.JSONObject
 
 class EditReplyActivity : BaseActivity() {
 
@@ -17,6 +20,35 @@ class EditReplyActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        okBtn.setOnClickListener {
+
+            val inputContent = contentEdt.text.toString()
+
+            ServerUtil.postRequestReply(mContext, mSelectedSide.topicId, inputContent, object : ServerUtil.Companion.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    val code = jsonObj.getInt("code")
+
+                    if(code == 200) {
+
+                        runOnUiThread{
+                            Toast.makeText(mContext, "의견 등록에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+                    }
+                    else {
+
+                        val message = jsonObj.getString("message")
+                        runOnUiThread{
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+
+            })
+        }
     }
 
     override fun setValues() {
